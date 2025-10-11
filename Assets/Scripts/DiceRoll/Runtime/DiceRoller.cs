067d5c27a95f179d7d03d9db4f4f597bb4f0d95e
IAFahim.smt <iafahim.dev@gmail.com>
2025-10-11 15:06:45 +0600

    Making it more clean

--- Assets/Scripts/DiceRoll/Runtime/DiceRoller.cs
+++ Assets/Scripts/DiceRoll/Runtime/DiceRoller.cs
@@ -4,106 +4,93 @@ using Random = UnityEngine.Random;
 
 namespace DiceRoll.Runtime
 {
-    [RequireComponent(typeof(Rigidbody))]
-    public class DiceRoller : MonoBehaviour
-    {
-        [Header("Roll Settings")] [SerializeField]
-        private bool initRoll;
-
-        [SerializeField] private bool random = true;
-        [SerializeField] private RollConfig rollConfig = RollConfig.Default();
-
-        [Header("Physics Settings")] [SerializeField]
-        private Rigidbody rb;
-
-        [Header("Debug")] [SerializeField] private bool showDebug = true;
-        [SerializeField] private Vector3 restPosition;
-
-        private void OnValidate()
-        {
-            rb ??= GetComponent<Rigidbody>();
-        }
-
-        [ContextMenu("Roll")]
-        public void Roll()
-        {
-            PlaceAgain();
-            if (random)
-            {
-                RandomPhysis.DirectionTorque(out rollConfig.rotation, out rollConfig.torque);
-            }
-
-            AddForceInDirection(transform, rb,
-                rollConfig.rollForce, rollConfig.upwardForce, rollConfig.torqueForce,
-                rollConfig.direction, rollConfig.rotation, rollConfig.torque
-            );
-        }
-
-        [ContextMenu("PlaceAgain")]
-        public void PlaceAgain()
-        {
-            transform.position = restPosition;
-        }
-
-        private void FixedUpdate()
-        {
-            if (initRoll)
-            {
-                Roll();
-                initRoll = false;
-            }
-        }
-
-        private void AddForceInDirection(
-            Transform transform, Rigidbody rb,
-            in float rollForce, in float upwardForce, in float torqueForce,
-            in Vector3 rollDirection, in Quaternion rotation, in Vector3 torqueDirection
-        )
-        {
-            rb.linearVelocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
-            transform.rotation = rotation;
-            var force = rollDirection * rollForce + Vector3.up * upwardForce;
-            rb.AddForce(force, ForceMode.Impulse); 
-            var torque = torqueDirection * torqueForce;
-            rb.AddTorque(torque, ForceMode.Impulse);
-        }
-    }
-
     [Serializable]
     public struct RollConfig
     {
         public float rollForce;
         public float torqueForce;
         public float upwardForce;
-
         public Vector3 direction;
         public Quaternion rotation;
         public Vector3 torque;
 
-        public static RollConfig Default()
+        public static RollConfig Default() => new()
         {
-            return new RollConfig()
-            {
-                rollForce = 15,
-                torqueForce = 2,
-                upwardForce = 3,
-                direction = new Vector3(0, 0, 1),
-                rotation = Quaternion.identity,
-                torque = Vector3.zero
-            };
-        }
+            rollForce = 15f,
+            torqueForce = 2f,
+            upwardForce = 3f,
+            direction = new(0, 0, 1),
+            rotation = Quaternion.identity,
+            torque = Vector3.zero
+        };
     }
-    
-    public static class RandomPhysis{
-        public static void DirectionTorque(out Quaternion rotation, out Vector3 torque)
+
+    public static class DicePhysics
+    {
+        public static void ApplyRollForce(
+            Rigidbody rb,
+            Transform transform,
+            in RollConfig config)
         {
-            rotation = Random.rotation;
-            torque = new Vector3(
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            transform.rotation = config.rotation;
+            
+            var force = config.direction * config.rollForce + Vector3.up * config.upwardForce;
+            rb.AddForce(force, ForceMode.Impulse);
+            
+            var torque = config.torque * config.torqueForce;
+            rb.AddTorque(torque, ForceMode.Impulse);
+        }
+
+        public static RollConfig RandomizeConfig(RollConfig config)
+        {
+            config.rotation = Random.rotation;
+            config.torque = new Vector3(
                 Random.Range(-1f, 1f),
                 Random.Range(-1f, 1f),
                 Random.Range(-1f, 1f)
             ).normalized;
+            return config;
+        }
+    }
+
+    [RequireComponent(typeof(Rigidbody))]
+    public class DiceRoller : MonoBehaviour
+    {
+        [SerializeField] private bool roll = true;
+        [SerializeField] private bool randomize = true;
+        [SerializeField] private Vector3 spawnPosition;
+        [SerializeField] private RollConfig rollConfig = RollConfig.Default();
+
+        private Rigidbody rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+            spawnPosition = transform.position;
+        }
+
+        private void FixedUpdate()
+        {
+            ResetPosition();
+            if (roll)
+            {
+                Roll();
+                roll = false;
+            }
+        }
+
+        public void Roll()
+        {
+            ResetPosition();
+            var config = randomize ? DicePhysics.RandomizeConfig(rollConfig) : rollConfig;
+            DicePhysics.ApplyRollForce(rb, transform, config);
+        }
+
+        public void ResetPosition()
+        {
+            transform.position = spawnPosition;
         }
     }
 }
