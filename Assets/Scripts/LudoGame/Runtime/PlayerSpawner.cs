da412c96499b322a3fad8d3bc4e34ffa479b7ca3
IAFahim <ishtiaq.fahim@northsouth.edu>
2025-11-03 21:03:58 +0600

    update ludo

--- Assets/Scripts/LudoGame/Runtime/PlayerSpawner.cs
+++ Assets/Scripts/LudoGame/Runtime/PlayerSpawner.cs
@@ -12,8 +12,8 @@ namespace Placements.Runtime
             // Instantiate PawnBase prefabs at the sorted positions
             if (playerCount == 2)
             {
-                tokenBases[1].gameObject.SetActive(false);
-                tokenBases[3].gameObject.SetActive(false);
+                tokenBases[0].gameObject.SetActive(true);
+                tokenBases[2].gameObject.SetActive(true);
                 return;
             }
             for (int i = 0; i < 4; i++)
@@ -21,7 +21,7 @@ namespace Placements.Runtime
                 var pawnBase = tokenBases[i];
                 if (i >= playerCount)
                 {
-                    pawnBase.gameObject.SetActive(false);
+                    pawnBase.gameObject.SetActive(true);
                 }
             }
             
